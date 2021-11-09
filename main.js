ac0cc388058fab6cb0d8ec62e6267e259fbd72a1
Tiffany Giordano <tiffany.giordano.j0ho@statefarm.com>
2021-11-09 11:23:34 -0600

    added acl parameter

--- main.js
+++ main.js
@@ -4,14 +4,15 @@ import { s3Client } from "./client.js";
 
 // Set the parameters
 const params = {
-  Bucket: "<BUCKET_NAME>", // The name of the bucket. For example, 'sample_bucket_101'.
-  Key: "<FILENAME>", // The name of the object. For example, 'sample_upload.txt'.
-  Body: "<BODY_CONTENT>", // The content of the object. For example, 'Hello world!".
+  Bucket: 'tagiordano2021t3', // The name of the bucket. For example, 'sample_bucket_101'.
+  Key: 'sample_upload.txt', // The name of the object. For example, 'sample_upload.txt'.
+  Body: 'This is my first attempt. Hi!', // The content of the object. For example, 'Hello world!".
+  ACL: 'public-read'
 };
 
 const run = () => {
   s3Client
-    .send(new CreateBucketCommand({ Bucket: params.Bucket }))
+    .send(new CreateBucketCommand({ Bucket: params.Bucket}))
     .then((data) => {
       console.log("Successfully created a bucket called ", data.Location);
     })
